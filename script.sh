312e4668dc15312c00d229d8c9121ad85bc4268c
HackPenguin <tdr.vrossi@gmail.com>
2024-02-19 17:33:57 -0500

    se realiza el ingreso de una 4ta funcion

---
+++ script.sh
@@ -0,0 +1,19 @@
+#!bin/bash/
+
+#traces the route between an origin and a destination.
+
+tracert "www.wikipedia.com"
+
+#The command displays the IP address, subnet mask and gateway
+
+# test
+
+ipconfig
+
+#determines DNS problems.
+
+nslookup "www.google.com"
+
+#monitoreo de rutas de red
+
+tracert -d -h 30
